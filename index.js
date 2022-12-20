158eea2b178fd25b1f3a8320d21e84ce2deb250a
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2022-12-20 02:36:07 -0800

    commit tree 32f9661a282a9ac468ee01b3bd5d9ba4e629155f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr,msg) {
+    const newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${msg} gift!`);
+    }
+    return newArr;
+}
+
+function countDown(n) {
+    let i = n
+    while(i >= 0) {
+        console.log(i--);
+    }
+}
